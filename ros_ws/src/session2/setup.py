6cde5017494c2f4f89f0e7c9fb5e5db4d2a9f7b4
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2026-08-29 23:16:25 +0300

    session 2 part 2
    params and  launch files

--- ros_ws/src/session2/setup.py
+++ ros_ws/src/session2/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'session2'
 
@@ -10,6 +11,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -28,6 +32,7 @@ setup(
             'service_client = session2.service_client:main',
             'action_server = session2.action_server:main',
             'action_client = session2.action_client:main',
+            'general_node = session2.general_node:main',
         ],
     },
 )
